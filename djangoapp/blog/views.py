a658514eabcbd61352daa2af73264dbbf7c754ab
Lucas Martins <127567616+LucasPMartins@users.noreply.github.com>
2025-01-07 07:37:43 -0300

    Refactor post template and views for improved content display; add created_by, category, and tag views with pagination; update URL patterns and adjust server run command.

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,31 +1,90 @@
-from django.shortcuts import render
+from blog.models import Post,Tag
 from django.core.paginator import Paginator
-from blog.models import Post
+from django.shortcuts import render
 
 PER_PAGE = 9
 
 def index(request):
     posts = Post.objects.get_published() # type: ignore
 
-    paginator = Paginator(posts,PER_PAGE)
-    page_number = request.GET.get('page')
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
     return render(
         request,
-         'blog/pages/index.html',
-        context={
+        'blog/pages/index.html',
+        {
             'page_obj': page_obj,
         }
     )
 
-def page(request):
+def created_by(request, author_pk):
+    posts = Post.objects.get_published().filter(created_by__pk=author_pk) # type: ignore
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+def category(request, slug):
+    posts = Post.objects.get_published().filter(category__slug=slug) # type: ignore
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+def tag(request, slug):
+    tag_pk = Tag.objects.filter(slug=slug).first().pk # type: ignore
+    posts = Post.objects.get_published().filter(tags__pk=tag_pk) # type: ignore
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+def page(request, slug):
     return render(
         request,
         'blog/pages/page.html',
-        
-        )
+        {
+            # 'page_obj': page_obj,
+        }
+    )
 
-def post(request):
-    return render(request, 'blog/pages/post.html')
+def post(request, slug):
+    post = (
+        Post.objects.get_published() # type: ignore
+        .filter(slug=slug)
+        .first()
+    )
 
+    return render(
+        request,
+        'blog/pages/post.html',
+        {
+            'post': post,
+        }
+    )
