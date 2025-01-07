35a7e848b0d5e88345efc0f8bcc66f156af50c69
Lucas Martins <127567616+LucasPMartins@users.noreply.github.com>
2025-01-07 11:21:47 -0300

    modifing rands

--- djangoapp/utils/rands.py
+++ djangoapp/utils/rands.py
@@ -1,9 +1,13 @@
-import random
 import string
+from random import SystemRandom
+
 from django.utils.text import slugify
 
-def random_letters(length):
-    return ''.join(random.choices(string.ascii_letters, k=length))
+def random_letters(k=5):
+    return ''.join(SystemRandom().choices(
+        string.ascii_lowercase + string.digits,
+        k=k
+    ))
 
 def new_slugify(string,k=5):
     return slugify(string) + '-' + random_letters(k)
